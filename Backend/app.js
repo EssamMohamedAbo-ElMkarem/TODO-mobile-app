73c94d293f8c43456e86b233958ad5aab61600c3
Essam Mohamed <50668640+EssamMohamedAbo-ElMkarem@users.noreply.github.com>
2023-06-14 15:10:53 +0300

    Express.js backend commit

---
+++ Backend/app.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const bcrypt = require("bcrypt");
+const User = require("./db/userModel");
+const http = require('http').createServer(app).listen(3000);
+
+require('dotenv').config()
+
+mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+
+db = mongoose.connection;
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// body parser configuration
+app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
+});
+
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  console.log(request.body)
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+        // create a new user instance and collect the data
+        const user = new User({
+          email: request.body.email,
+          password: hashedPassword,
+          tasks: request.body.tasks
+        });
+        db.collection('users').insertOne(user, function (error, res) {
+          if(error) {
+            response.status(400).send(res);
+          } else {
+            response.status(200).send(res);
+          }
+        });
+    });
+});
+
+// login endpoint
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ email: request.body.email })
+
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        // if the passwords match
+        .then((passwordCheck) => {
+
+          // check if password matches
+          if(!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+
+          //   return success response
+          response.status(200).send({
+            message: "Login Successful",
+            tasks: user.tasks,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
+});
+
+
+module.exports = app;
